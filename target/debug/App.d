0ba10ee587bbc5367543931997584786a4c326e6
leo-1228 <akiraito731@gmail.com>
2024-05-26 07:24:24 -0400

    Helo, world!

---
+++ target/debug/App.d
@@ -0,0 +1,1 @@
+E:\study\egui\App\target\debug\App.exe: E:\study\egui\App\src\main.rs
---
+++ target/debug/deps/App.d
@@ -0,0 +1,5 @@
+E:\study\egui\App\target\debug\deps\App.exe: src\main.rs
+
+E:\study\egui\App\target\debug\deps\App.d: src\main.rs
+
+src\main.rs:
